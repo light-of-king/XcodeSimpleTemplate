d70aa36bcb03380fff349bdcc44d5170d3d4fa38
light-of-king <king@weijiang.local>
2016-07-22 16:30:01 +0800

    简单模板

---
+++ Category/UIView+Frame.h
@@ -0,0 +1,23 @@
+//
+//  UIView+Frame.h
+//  test
+//
+//  Created by king on 16/7/20.
+//  Copyright © 2016年 King. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (Frame)
+
+@property (nonatomic,assign) CGFloat x;
+@property (nonatomic,assign) CGFloat y;
+@property (nonatomic,assign) CGFloat width;
+@property (nonatomic,assign) CGFloat height;
+@property (nonatomic,assign) CGFloat centerX;
+@property (nonatomic,assign) CGFloat centerY;
+@property (nonatomic,assign) CGSize size;
+@property (nonatomic,assign) CGPoint origin;
+
+
+@end
